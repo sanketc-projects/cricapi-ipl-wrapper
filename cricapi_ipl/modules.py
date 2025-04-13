bb619f8a7d558626361d14e7ef9e687f061ba8c4
Sanket Chitagopkar <sanket.tronics@gmail.com>
2025-04-12 19:57:56 -0700

    Match list

--- cricapi_ipl/modules.py
+++ cricapi_ipl/modules.py
@@ -6,9 +6,32 @@ from datetime import datetime
 
 
 API_KEY = ""
-SERIES_SERACH_URL = "https://api.cricapi.com/v1/series"
 SERIES_SEARCH_TERM = "Indian Premier League"
 
+# API URLs
+SERIES_SERACH_URL = "https://api.cricapi.com/v1/series"
+SERIES_INFO_URL = "https://api.cricapi.com/v1/series_info"
+
+
+class Match:
+    def __init__(self, match_json):
+        self.id = match_json.get("id", "N/A")
+        self.name = match_json.get("name", "N/A")
+        date_str = match_json.get("date")
+        try:
+            self.date = datetime.strptime(date_str, "%Y-%m-%d")
+        except ValueError:
+            self.date = datetime.now()
+        self.status = match_json.get("status", "N/A")
+        self.venue = match_json.get("venue", "N/A")
+        self.home_team = match_json.get("teams", ["N/A", "N/A"])[0]
+        self.away_team = match_json.get("teams", ["N/A", "N/A"])[1]
+
+    def __str__(self):
+        return f"{self.name:<40}, Date: {self.date.strftime('%B %d %Y'):<15}, Status: {self.status:<10}"
+
+    def __repr__(self):
+        return self.__str__()
 
 class Series:
     def __init__(self, series_json):
@@ -26,12 +49,31 @@ class Series:
             self.end_date = datetime.strptime(end_date_str_formatted, "%B %d %Y")
         except ValueError:
             self.end_date = datetime.now()
+        self.matches = []
 
     def __str__(self):
-        return f"{self.name:<40}, Matches: {self.num_matches:<10}, Start Date: {self.start_date:<15}, End Date: {self.end_date:15}"
+        return f"{self.name:<40}, Matches: {self.num_matches:<10}, Start Date: {self.start_date.strftime('%B %d %Y'):<15}, End Date: {self.end_date.strftime('%B %d %Y'):15}"
 
     def __repr__(self):
         return self.__str__()
+
+    def update_matches(self):
+        if not API_KEY:
+            raise ValueError("API key is not set. Use set_api_key() to set it.")
+
+        params = {
+            "apikey": API_KEY,
+            "id": self.id
+        }
+        response = requests.get(SERIES_INFO_URL, params=params, timeout=10)
+        response.raise_for_status()
+        series_data = response.json().get("data", {})
+        all_results = series_data.get("matchList", [])
+
+        info = response.json().get("info", {})
+        _update_hits_info(info)
+
+        self.matches = [Match(match) for match in all_results]
 
 class HitInfo:
     def __init__(self):
@@ -58,6 +100,10 @@ def set_api_key(api_key):
     if not pattern.match(api_key):
         raise ValueError("API key must be in GUID format.")
     API_KEY = api_key
+
+def clear_api_key():
+    global API_KEY
+    API_KEY = ""
 
 def get_hits_info():
     return hits
--- tests/test_get_series_list.py
+++ tests/test_get_series_list.py
@@ -4,7 +4,6 @@ from unittest.mock import patch, Mock
 import pytest
 
 
-
 def test_get_series_list_no_api_key():
     with pytest.raises(ValueError):
         get_series_list()  # Should raise ValueError if API key is not set
@@ -65,4 +64,95 @@ def test_get_series_list():
         hits = get_hits_info()
         assert hits.hits_today == 1
         assert hits.hits_used == 1
+        assert hits.hits_limit == 100
+
+def test_update_match_info():
+    mock_response = Mock()
+    mock_response.json.return_value = {
+        "data": {
+            "info": {
+              "id": "d5a498c8-7596-4b93-8ab0-e0efc3345312",
+              "name": "Indian Premier League 2025",
+              "startdate": "2025-03-22",
+              "enddate": "May 25",
+              "odi": 0,
+              "t20": 74,
+              "test": 0,
+              "squads": 10,
+              "matches": 74
+            },
+            "matchList": [
+                {
+                    "id": "208d68e5-3fab-4f3b-88e9-29ec4a02d3e2",
+                    "name": "Chennai Super Kings vs Mumbai Indians, 3rd Match",
+                    "matchType": "t20",
+                    "status": "Chennai Super Kings won by 4 wkts",
+                    "venue": "MA Chidambaram Stadium, Chennai",
+                    "date": "2025-03-23",
+                    "dateTimeGMT": "2025-03-23T14:00:00",
+                    "teams": [
+                      "Chennai Super Kings",
+                      "Mumbai Indians"
+                    ]
+                },
+                {
+                    "id": "83d70527-5fc4-4fad-8dd2-b88b385f379e",
+                    "name": "Gujarat Titans vs Punjab Kings, 5th Match",
+                    "matchType": "t20",
+                    "status": "Punjab Kings won by 11 runs",
+                    "venue": "Narendra Modi Stadium, Ahmedabad",
+                    "date": "2025-03-25",
+                    "dateTimeGMT": "2025-03-25T14:00:00",
+                    "teams": [
+                      "Gujarat Titans",
+                      "Punjab Kings"
+                    ]
+                },
+          ]
+        },
+        "info": {
+            "hitsToday": 2,
+            "hitsUsed": 2,
+            "hitsLimit": 100,
+            "credits": 0,
+            "server": 8,
+            "offsetRows": 0,
+            "totalRows": 4,
+            "queryTime": 43.9282,
+            "s": 0,
+            "cache": 0
+        }
+    }
+    mock_response.raise_for_status = Mock()
+    mock_response.status_code = 200
+
+    with patch('cricapi_ipl.modules.requests.get', return_value=mock_response):
+        series = Series({
+            "id": "1",
+            "name": "Indian Premier League 2023",
+            "matches": 70,
+            "startDate": "2023-03-31",
+            "endDate": "May 15"
+        })
+        series.update_matches()
+        assert len(series.matches) == 2
+        assert series.matches[0].name == "Chennai Super Kings vs Mumbai Indians, 3rd Match"
+        assert series.matches[0].status == "Chennai Super Kings won by 4 wkts"
+        assert series.matches[0].venue == "MA Chidambaram Stadium, Chennai"
+        assert series.matches[0].date == datetime.strptime("2025-03-23", "%Y-%m-%d")
+        assert series.matches[0].home_team == "Chennai Super Kings"
+        assert series.matches[0].away_team == "Mumbai Indians"
+        assert series.matches[0].id == "208d68e5-3fab-4f3b-88e9-29ec4a02d3e2"
+
+        assert series.matches[1].name == "Gujarat Titans vs Punjab Kings, 5th Match"
+        assert series.matches[1].status == "Punjab Kings won by 11 runs"
+        assert series.matches[1].venue == "Narendra Modi Stadium, Ahmedabad"
+        assert series.matches[1].date == datetime.strptime("2025-03-25", "%Y-%m-%d")
+        assert series.matches[1].home_team == "Gujarat Titans"
+        assert series.matches[1].away_team == "Punjab Kings"
+        assert series.matches[1].id == "83d70527-5fc4-4fad-8dd2-b88b385f379e"
+
+        hits = get_hits_info()
+        assert hits.hits_today == 2
+        assert hits.hits_used == 2
         assert hits.hits_limit == 100
